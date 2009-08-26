d25d8c007b7272ab775525c0f124c2fc6db00999
Brent Shaffer <bshafs@gmail.com>
2009-08-26 16:12:23 -0500

    better formatting for accounts module in backend

--- apps/backend/templates/_admin_menu.php
+++ apps/backend/templates/_admin_menu.php
@@ -3,6 +3,7 @@
 		<li><?php echo link_to('Plugins', '@symfony_plugin') ?></li>	  
 		<li><?php echo link_to('Categories', '@plugin_category') ?></li>	  
 		<li><?php echo link_to('Authors', '@plugin_author') ?></li>	  
+		<li><?php echo link_to('Accounts', '@plugin_author') ?></li>	  		
 		<li><?php echo link_to('Approve Comments', '@comment_csCommentAdmin') ?></li>
 	</ul>	
 <?php endif ?>
