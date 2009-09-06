bfc5adab6e6c0d732e074e77cc25f2c99ef96a22
Brent Shaffer <bshafs@gmail.com>
2009-09-06 09:28:46 -0500

    nav changes

--- apps/frontend/modules/author/templates/_user_links.php
+++ apps/frontend/modules/author/templates/_user_links.php
@@ -1,4 +1,3 @@
-<li><?php echo link_to('view profile', '@author?username='.$sf_user->getGuardUser()->getUsername()) ?></li>
-<li><?php echo link_to('edit profile', '@author_edit?username='.$sf_user->getGuardUser()->getUsername()) ?></li>
+<li><?php echo link_to('my profile', '@author?username='.$sf_user->getGuardUser()->getUsername()) ?></li>
 <li><?php echo link_to('register a plugin', '@plugin_register') ?></li>
 <li><?php echo link_to('sign out', '@signout_redirect') ?></li>
