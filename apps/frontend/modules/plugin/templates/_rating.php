9f5ebb0a7b4f7a767ab3dbd18058b27c80b47e16
Brent Shaffer <bshafs@gmail.com>
2009-12-14 17:35:21 -0600

    addresses issue with stars not showing in firefox

--- apps/frontend/modules/plugin/templates/_rating.php
+++ apps/frontend/modules/plugin/templates/_rating.php
@@ -1,13 +1,12 @@
-<?php $attributes = array('background-position-y:-16px',
+<?php $attributes = array('background-position:0px -16px',
                           'display:block',
                           'float:left',
                           'width:15px', 
                           'height:15px', 
-                          'background-image:url('.image_path('star', array('absolute' => true)).')',
-                          'background-position-x:0px') ?>
+                          'background-image:url('.image_path('star', array('absolute' => true)).')') ?>
 <?php for($i = 1; $i <= 5; $i++): ?>
   <?php if ($rating < $i): ?>
-    <?php $attributes[0] = 'background-position-y:0px' ?>
+    <?php $attributes[0] = 'background-position:0px 0px' ?>
   <?php endif ?>
   <div style='<?php echo implode(';', $attributes) ?>'></div>
 <?php endfor ?>
