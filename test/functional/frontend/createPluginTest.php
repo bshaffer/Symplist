3d8d98a179c95892719fa67c3ae7e9eb747a9a12
Brent Shaffer <bshafs@gmail.com>
2010-06-06 18:05:45 -0500

    test cleanup and fixes.  All tests pass

--- test/functional/frontend/createPluginTest.php
+++ test/functional/frontend/createPluginTest.php
@@ -3,52 +3,55 @@
 include(dirname(__FILE__).'/../../bootstrap/functional.php');
 include(dirname(__FILE__).'/../../fixtures/fixtures.php');
 
-$browser = new sfTestFunctional(new sfBrowser());
+$browser = new csTestFunctional(new sfBrowser());
 $plugin = 'sfFakePlugin'.rand();
 
-$browser->
-  get('/')->
-  click('Sign In')->
-  setField('signin[username]', 'test')->
-  setField('signin[password]', 'test')->
+$browser
+  ->get('/')
+  ->click('Sign In')
+  ->setField('signin[username]', 'test')
+  ->setField('signin[password]', 'test')
   
-  click('sign in')->
+  ->click('sign in')
   
-  followRedirect()->
+  ->followRedirect()
   
-  click('register a plugin')->
+  ->click('Plugins')
+    ->isModuleAction('plugin', 'list')
+  
+  ->click('Register a Plugin')
+    ->isModuleAction('plugin', 'register')
     
-    setField('symfony_plugin[title]', $plugin)->
-    setField('symfony_plugin[description]', 'description')->  
-    setField('symfony_plugin[repository]', 'kasjdjfds')->  
+  ->setField('symfony_plugin[title]', $plugin)
+  ->setField('symfony_plugin[description]', 'description')
+  ->setField('symfony_plugin[repository]', 'kasjdjfds')
     
-    click('Submit')->
+  ->click('Submit')
     
-    with('form')->begin()->
-      hasErrors()->
-    end()->
+  ->with('form')->begin()
+    ->hasErrors()
+  ->end()
     
-    setField('symfony_plugin[title]', $plugin)->
-    setField('symfony_plugin[description]', 'description')->  
-    setField('symfony_plugin[repository]', 'http://repository.com')->  
+  ->setField('symfony_plugin[title]', $plugin)
+  ->setField('symfony_plugin[description]', 'description')
+  ->setField('symfony_plugin[repository]', 'http://repository.com')
     
-    click('Submit')->
+  ->click('Submit')
     
-    with('form')->begin()->
-      hasErrors(false)->
-    end()->
+  ->with('form')->begin()
+    ->hasErrors(false)
+  ->end()
     
-    isRedirected()->
-    followRedirect()->
+  ->isRedirected()
+  ->followRedirect()
     
-    with('request')->begin()->
-      isParameter('module', 'plugin')->
-      isParameter('action', 'show')->
-      isParameter('title', $plugin)->
-    end()->
+  ->with('request')->begin()
+    ->isParameter('module', 'plugin')
+    ->isParameter('action', 'show')
+    ->isParameter('title', $plugin)
+  ->end()
     
-    with('response')->begin()->
-      isStatusCode(200)->
-    end()
-      
+  ->with('response')->begin()
+    ->isStatusCode(200)
+  ->end()  
 ;
--- test/functional/frontend/registerTest.php
+++ test/functional/frontend/registerTest.php
@@ -2,103 +2,110 @@
 
 include(dirname(__FILE__).'/../../bootstrap/functional.php');
 
-$browser = new sfTestFunctional(new sfBrowser());
+$browser = new csTestFunctional(new sfBrowser(), null, array('form' => 'csTesterForm'));
 $username = 'brohan'.rand();
-$browser->
-  get('/')->
-  click('Register')->
+$browser
+  ->get('/')
+  ->click('Register')
 
-  with('request')->begin()->
-    isParameter('module', 'author')->
-    isParameter('action', 'join')->
-  end()->
+  ->with('request')->begin()
+    ->isParameter('module', 'author')
+    ->isParameter('action', 'join')
+  ->end()
   
-  with('response')->begin()->
-    isStatusCode(200)->
-    contains('Step 1:')->
-  end()->
+  ->with('response')->begin()
+    ->isStatusCode(200)
+    ->contains('Step 1:')
+  ->end()
     
-  setField('sf_guard_user[username]', $username)->
-  setField('sf_guard_user[password]', 'password')->
-  setField('sf_guard_user[password_again]', 'password')->  
+  ->setField('sf_guard_user[username]', $username)
+  ->setField('sf_guard_user[password]', 'password')
+  ->setField('sf_guard_user[password_again]', 'password')
   
-  click('Submit')->
+  ->click('Submit')
   
-  with('request')->begin()->
-    isParameter('module', 'author')->
-    isParameter('action', 'join')->
-  end()->
+  ->with('request')->begin()
+    ->isParameter('module', 'author')
+    ->isParameter('action', 'join')
+  ->end()
   
-  with('response')->begin()->
-    isStatusCode(200)->
-    info('ensure the user is directed to step two')->
-    contains('Step 2:')->
+  ->with('response')->begin()
+    ->isStatusCode(200)
+    ->info('ensure the user is directed to step two')
+    ->contains('Step 2:')
 
-    info('ensure the system logs the user in')->
-    contains('Sign Out')->
-  end()->
+    ->info('ensure the system logs the user in')
+    ->contains('Sign Out')
+  ->end()
   
-  setField('plugin_author[first_name]', 'Fake')->
-  setField('plugin_author[last_name]', 'Guy')->
-  setField('plugin_author[email]', 'fake.guy@gmail.com')->  
-  setField('plugin_author[bio]', 'I am not fake, despite my unfortunate birth name.  Thanks mom.')->
+  ->setField('plugin_author[first_name]', 'Fake')
+  ->setField('plugin_author[last_name]', 'Guy')
+  ->setField('plugin_author[email]', 'fake.guy@gmail.com')
+  ->setField('plugin_author[bio]', 'I am not fake, despite my unfortunate birth name.  Thanks mom.')
   
-  click('Submit')->
+  ->click('Submit')
   
-  with('request')->begin()->
-    isParameter('module', 'author')->
-    isParameter('action', 'join')->
-  end()->
+  ->with('request')->begin()
+    ->isParameter('module', 'author')
+    ->isParameter('action', 'join')
+  ->end()
   
-  with('response')->begin()->
-    isStatusCode(200)->
-    contains('You have successfully created your account')->
-  end()->
+  ->with('response')->begin()
+    ->isStatusCode(200)
+    ->contains('You have successfully created your account')
+  ->end()
   
-  click('here')->
+  ->click('here')
   
-  with('request')->begin()->
-    isParameter('module', 'author')->
-    isParameter('action', 'show')->
-  end()->
+  ->with('request')->begin()
+    ->isParameter('module', 'author')
+    ->isParameter('action', 'show')
+  ->end()
   
-  with('response')->begin()->
-    isStatusCode(200)->
-    contains($username)->
-    contains('edit')->
-  end()->
+  ->with('response')->begin()
+    ->isStatusCode(200)
+    ->contains($username)
+    ->contains('edit')
+  ->end()
   
-  click('edit')->
+  ->click('edit')
   
-  with('request')->begin()->
-    isParameter('module', 'author')->
-    isParameter('action', 'edit')->
-  end()->
+  ->with('request')->begin()
+    ->isParameter('module', 'author')
+    ->isParameter('action', 'edit')
+  ->end()
   
-  with('response')->begin()->
-    isStatusCode(200)->
-  end()->
+  ->with('response')->begin()
+    ->isStatusCode(200)
+    ->checkForm('sfGuardUserAdminForm')
+    ->checkForm('PluginAuthorForm')
+  ->end()
   
-  setField('plugin_author[email]', 'invalidemail')->  
+  ->setField('plugin_author[email]', 'invalidemail')
   
-  click('Save')->
+  ->click('Save')
   
-  with('response')->begin()->
-    contains('invalid')->
-  end()->
+  ->with('form')->begin()
+    ->setFormForPosition(2)
+    ->hasErrors()
+  ->end()
   
-  setField('plugin_author[email]', 'valid.email@gmail.com')->  
-  setField('plugin_author[first_name]', 'TestFirstName')->  
+  ->with('response')->begin()
+    ->contains('invalid')
+  ->end()
+  
+  ->setField('plugin_author[email]', 'valid.email@gmail.com')
+  ->setField('plugin_author[first_name]', 'TestFirstName')
 
-  click('Save')->
+  ->click('Save')
   
-  with('form')->begin()->
-    hasErrors(false)->
-  end()->
+  ->with('form')->begin()
+    ->hasErrors(false)
+  ->end()
   
-  click('My Profile')->
+  ->click('My Profile')
   
-  with('response')->begin()->
-    contains('TestFirstName')->
-  end()
+  ->with('response')->begin()
+    ->contains('TestFirstName')
+  ->end()
 ;
